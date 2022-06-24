81b875bf1c64b6cd53d6a2ee5078a31d0bd2aa12
BytesLiang <xinzhu_liang@163.com>
2022-06-24 14:57:36 +0800

    统一异常处理

---
+++ common/service_base/src/main/java/com/liang/service/base/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.liang.service.base.exceptionHandler;
+
+import com.liang.common.utils.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public Result<Object> error(Exception e){
+        log.error(e.getMessage());
+        return Result.error().setMessage("GlobalException");
+    }
+
+    @ExceptionHandler(ArithmeticException.class)
+    public Result<Object> error(ArithmeticException e){
+        log.error(e.getMessage());
+        return Result.error().setMessage("ArithmeticException");
+    }
+
+    @ExceptionHandler(MyException.class)
+    public Result<Object> error(MyException e){
+        log.error(e.getMessage());
+        return Result.error()
+                .setCode(e.getCode())
+                .setMessage(e.getMsg());
+    }
+}
